08828843e2595e11620ea019703bd46213152fe5
Jinho-Bae <baejin922@gmail.com>
2022-08-12 19:49:08 +0900

    4.3 Home

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import styled, {keyframes} from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
+function App() {  
+  return <Wrapper>
+    <Box>
+      <Emoji as="p">hey</Emoji>
+    </Box>
+  </Wrapper>;
+}
+
+export default App;
